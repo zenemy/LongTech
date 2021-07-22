013b55865c3e3cc9a7b1a2c490e4ff2b77b34f0d
awalmlna <awal.maulana@gmail.com>
2021-07-22 21:13:46 +0700

    Lanjut Setting layout2 Car Log

--- app/src/main/java/com/julong/longtech/menuvehicle/KartuKerjaVehicle.java
+++ app/src/main/java/com/julong/longtech/menuvehicle/KartuKerjaVehicle.java
@@ -1,11 +1,18 @@
 package com.julong.longtech.menuvehicle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.julong.longtech.DatabaseHelper;
 import com.julong.longtech.R;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.KeyListener;
@@ -19,6 +26,7 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Calendar;
@@ -30,15 +38,17 @@ public class KartuKerjaVehicle extends AppCompatActivity {
 
     String savedate;
     private KeyListener keyListenerEtHasilKerja;
+    byte[] gambarCarLog;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     EditText etHasilKerjaCarLog;
     TextView tvTanggalCarLog, tvInfoUnitCarLog;
     AutoCompleteTextView acLoadTypeCarLog, acHelper1CarLog, acHelper2CarLog, acLoadCategoryCarLog;
     LinearLayout layoutHelperCarlog, layoutAsalCarLog, layoutTujuanCarLog;
     Button btnSubmitCarlog, btnBackCarLog;
-    ImageButton btnAddHasilKerja, btnMinusHasilKerja;
-    TextInputLayout inputLayoutAsalKebun, inputLayoutAsalDivisi, inputLayoutAsalLokasi, inputLayoutTujuanKebun,
-            inputLayoutTujuanDivisi, inputLayoutTujuanLokasi, inputLayoutTujuanKegiatan, inputLayoutHasilKerjaCarLog;
+    ImageButton btnAddHasilKerja, btnMinusHasilKerja, btnCameraCarLog;
+    TextInputLayout inputLayoutAsalKebun, inputLayoutAsalDivisi, inputLayoutAsalLokasi, inputLayoutTujuanKebun, inputLayoutHelper1CarLog,
+            inputLayoutHelper2CarLog, inputLayoutTujuanDivisi, inputLayoutTujuanLokasi, inputLayoutTujuanKegiatan, inputLayoutHasilKerjaCarLog;
 
     private List<String> listMuatanCarLog, listEmployee, listCategoryMuatan;
     ArrayAdapter<String> adapterMuatanCarLog, adapterEmployee, adapterCategoryMuatan;
@@ -56,6 +66,8 @@ public class KartuKerjaVehicle extends AppCompatActivity {
         layoutAsalCarLog = findViewById(R.id.layoutAsalMuatanCarLog);
         layoutTujuanCarLog = findViewById(R.id.layoutTujuanMuatanCarLog);
         layoutHelperCarlog = findViewById(R.id.layoutHelperCarlog);
+        inputLayoutHelper1CarLog = findViewById(R.id.inputLayoutHelper1CarLog);
+        inputLayoutHelper2CarLog = findViewById(R.id.inputLayoutHelper2CarLog);
         acLoadTypeCarLog = findViewById(R.id.acLoadTypeCarLog);
         acHelper1CarLog = findViewById(R.id.acHelper1CarLog);
         acHelper2CarLog = findViewById(R.id.acHelper2CarLog);
@@ -73,6 +85,7 @@ public class KartuKerjaVehicle extends AppCompatActivity {
         inputLayoutTujuanKegiatan = findViewById(R.id.inputLayoutTujuanKegiatan);
         etHasilKerjaCarLog = findViewById(R.id.etHasilKerjaCarLog);
         inputLayoutHasilKerjaCarLog = findViewById(R.id.inputLayoutHasilKerjaCarLog);
+        btnCameraCarLog = findViewById(R.id.imgCarLog);
         keyListenerEtHasilKerja = etHasilKerjaCarLog.getKeyListener();
 
         btnBackCarLog.setOnClickListener(v -> finish());
@@ -89,6 +102,14 @@ public class KartuKerjaVehicle extends AppCompatActivity {
         adapterEmployee = new ArrayAdapter<String>(this, R.layout.spinnerlist, R.id.spinnerItem, listEmployee);
         acHelper1CarLog.setAdapter(adapterEmployee);
         acHelper2CarLog.setAdapter(adapterEmployee);
+
+        btnCameraCarLog.setOnClickListener(v -> {
+            gambarCarLog = null;
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(this.getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
 
         acLoadTypeCarLog.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -109,11 +130,17 @@ public class KartuKerjaVehicle extends AppCompatActivity {
         acLoadCategoryCarLog.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (dbhelper.layoutsetting_carlog(0, adapterCategoryMuatan.getItem(position)).equals("Y")) {
+
+                if (dbhelper.layoutsetting_carlog(0, adapterCategoryMuatan.getItem(position)).equals("Y") && dbhelper.layoutsetting_carlog(1, adapterCategoryMuatan.getItem(position)).equals("Y")) {
                     layoutHelperCarlog.setVisibility(View.VISIBLE);
-                } else if (dbhelper.layoutsetting_carlog(0, adapterCategoryMuatan.getItem(position)).equals("N")) {
+                } else if (dbhelper.layoutsetting_carlog(0, adapterCategoryMuatan.getItem(position)).equals("Y") && dbhelper.layoutsetting_carlog(1, adapterCategoryMuatan.getItem(position)).equals("N")) {
+                    layoutHelperCarlog.setVisibility(View.VISIBLE);
+                    inputLayoutHelper2CarLog.setVisibility(View.GONE);
+                }
+                else if (dbhelper.layoutsetting_carlog(0, adapterCategoryMuatan.getItem(position)).equals("N") && dbhelper.layoutsetting_carlog(1, adapterCategoryMuatan.getItem(position)).equals("N")) {
                     layoutHelperCarlog.setVisibility(View.GONE);
                 }
+
 
                 //Layout Asal Muatan
                 if (dbhelper.layoutsetting_carlog(2, adapterCategoryMuatan.getItem(position)).equals("Y") && dbhelper.layoutsetting_carlog(3, adapterCategoryMuatan.getItem(position)).equals("Y") && dbhelper.layoutsetting_carlog(4, adapterCategoryMuatan.getItem(position)).equals("Y")) {
@@ -232,6 +259,21 @@ public class KartuKerjaVehicle extends AppCompatActivity {
             }
         });
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap photoCamera = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            photoCamera.compress(Bitmap.CompressFormat.JPEG, 80, stream);
+            gambarCarLog = stream.toByteArray();
+            Bitmap compressedBitmap = BitmapFactory.decodeByteArray(gambarCarLog, 0, gambarCarLog.length);
+            btnCameraCarLog.setImageBitmap(compressedBitmap);
+            btnCameraCarLog.setForeground(null);
+        }
     }
 
     void todayDate() {
